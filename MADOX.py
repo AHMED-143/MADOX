2b4e9fd42ead1563275fd752c4a21f169e0ba2c3
YASIN <127176409+AHMED-143@users.noreply.github.com>
2023-12-28 13:04:30 +0600

    Create MADOX.py

---
+++ MADOX.py
@@ -0,0 +1,15 @@
+import os, platform, time, sys
+os.system('https://www.facebook.com/Yasin.Arafat.xy?mibextid=ZbWKwL')
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+ 
+print('\033[1;91m[\033[1;97m●\033[1;91m]\033[1;92m CHECKING FOR UPDATES...')
+os.system('git pull --quiet 2>/dev/null')
+madox = platform.architecture()[0]
+if madox == '64bit':
+ print('\033[1;91m[\033[1;97m●\033[1;91m] \033[1;92mYOUR DEVICE IS 64 BIT')
+ import xdxn
+elif madox == '32bit':
+ print('\033[1;91m[\033[1;97m●\033[1;91m] \033[1;92mYOUR DEVICE IS 32 BIT !! SORRY 32 BIT NOT SUPPORTED ')
+ os.system('exit')
